f3e63d020f8cf832345ee6781298e76bbd6bb75d
zkvasov <zkvasov07@gmail.com>
2019-07-08 17:36:09 +0300

    Add files via upload

---
+++ RandomHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace RandomForFigures
+{
+    public class RandomHelper
+    {
+        private Random rand;
+
+        public RandomHelper()
+        {
+            rand = new Random();
+        }
+
+        public int GetRandomMove()
+        {
+            int[] numbers = { -4, -3, -2, -1, 1, 2, 3, 4 };
+            return numbers[this.rand.Next(0, 8)];
+        }
+
+        public Point GetRandomPosition(Point Pmax)
+        {
+            int x = this.rand.Next(0, Pmax.X - 50);
+            int y = this.rand.Next(0, Pmax.Y - 50);
+            return new Point(x, y);
+        }
+    }
+}
